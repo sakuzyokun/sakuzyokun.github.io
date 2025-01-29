3999bf982c209679039af2917f6411ee3e2fd6d7
sakuzyokun <sakuzyokun.sakuzyo0606@gmail.com>
2025-01-29 19:52:25 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+window.addEventListener('load', showMessageBox);
+
+function showMessageBox() {
+    const messageBox = document.getElementById('messageBox');
+    messageBox.style.display = 'block';
+    moveMessageBox();
+
+    setInterval(moveMessageBox, 1000);
+}
+
+function closeMessageBox() {
+    document.getElementById('messageBox').style.display = 'none';
+}
+
+function moveMessageBox() {
+    const messageBox = document.getElementById('messageBox');
+    const x = Math.floor(Math.random() * (window.innerWidth - messageBox.clientWidth));
+    const y = Math.floor(Math.random() * (window.innerHeight - messageBox.clientHeight));
+    messageBox.style.left = `${x}px`;
+    messageBox.style.top = `${y}px`;
+}
