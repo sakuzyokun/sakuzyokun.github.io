17cca290d1dd47a37fd27257da2b85c0d4e59423
sakuzyokun <sakuzyokun.sakuzyo0606@gmail.com>
2025-02-07 13:41:43 +0900

    Update scripts.js

--- error408/scripts.js
+++ error408/scripts.js
@@ -19,6 +19,23 @@ function repeatCloneWindow() {
         }
     }
 
+    function createClone1() {
+        if (count < 4) {
+            // クローンを作成
+            const clone = originalWindow.cloneNode(true);
+            document.body.appendChild(clone);
+
+            // クローンの位置を調整
+            clone.style.position = 'absolute';
+            clone.style.top = `calc(50% + ${count * 25}px)`;
+            clone.style.left = `calc(50% + ${count * 25}px)`;
+            clone.style.transform = 'translate(-50%, -50%)';
+
+            count++;
+            setTimeout(createClone, 150); // 0.15秒の待ち時間
+        }
+    }
+
     // 最初のウィンドウを削除
     originalWindow.remove();
 
@@ -29,6 +46,9 @@ function repeatCloneWindow() {
             console.log("音声の自動再生がブロックされました:", error);
         });
         createClone();
+        setTimeout(() => {
+            createClone1();
+        }, 800);
     }, 3000); // 3秒の遅延
 }
 
